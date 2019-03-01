4bf545bff87f85916ac0f6d053cec6afcd3ac9f1
spdoc <46932662+spdoc@users.noreply.github.com>
2019-03-01 20:13:02 +0530

    Updated

--- Infradown_update.sh
+++ Infradown_update.sh
@@ -1,19 +1,22 @@
-#***Steps to scrap all infra and remove InfyWiki:***
-ecs-cli compose --file docker-compose.yml down --cluster-config InfyWiki
-ecs-cli down --force
-#collect all task details in file
-aws ecs list-task-definitions | grep task-definition | cut -f2 -d"/" | tr -d "\"" | tr -d "\," > task_list
-#echo all tasks
-cat task_list
-#delete all tasks
-while read task; do aws ecs deregister-task-definition --task-definition $task >/dev/null; done < task_list
-
-
-
 #delete service steps:
-aws ecs update-service --service InfyWiki --cluster "InfyWiki" --task-definition InfyWiki:6 --desired-count 0
-aws ecs delete-service --service InfyWiki --cluster "InfyWiki"
-aws elb delete-load-balancer --load-balancer-name InfyWiki
-ecs-cli compose --file docker-compose.yml --project-name InfyWiki --verbose create
+aws rds delete-db-instance --db-instance-identifier hackathon --delete-automated-backups --skip-final-snapshot;
+aws ecs update-service --service InfyWiki --cluster "InfyWiki" --desired-count 0;
+aws ecs delete-service --service InfyWiki --cluster "InfyWiki";
+aws elb delete-load-balancer --load-balancer-name InfyWiki;
+#collect all task details in file
+aws ecs list-task-definitions | grep task-definition | cut -f2 -d"/" | tr -d "\"" | tr -d "\," > task_list;
+#echo all tasks
+cat task_list;
+#delete all tasks
+while read task; do aws ecs deregister-task-definition --task-definition $task >/dev/null; done < task_list;
+rm -rf task_list;
+ecs-cli compose --file docker-compose.yml down --cluster-config InfyWiki;
+ecs-cli down --force;
+sleep 300;
 
-aws ecs create-service --service-name "InfyWiki" --cluster "InfyWiki" --task-definition InfyWiki:11 --load-balancers "loadBalancerName=InfyWiki,containerName=mediawiki,containerPort=443" --desired-count 2 --deployment-configuration "maximumPercent=200,minimumHealthyPercent=50"
+#Update or scale service
+ecs-cli compose --file docker-compose.yml --project-name InfyWiki --verbose create > Taskdetails
+TD=$(grep -r InfyWiki: Taskdetails | cut -f5 -d"=" | tr -d "\"" | tr -d "\,")
+aws ecs update-service --service InfyWiki --cluster "InfyWiki" --desired-count 0
+aws ecs delete-service --service InfyWiki --cluster "InfyWiki"
+aws ecs create-service --service-name "InfyWiki" --cluster "InfyWiki" --task-definition $TD --load-balancers "loadBalancerName=InfyWiki,containerName=mediawiki,containerPort=443" --desired-count 2 --deployment-configuration "maximumPercent=200,minimumHealthyPercent=50"

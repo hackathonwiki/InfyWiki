be223d2be7092504f5dd9ad3aeddee3cc5b11599
spdoc <46932662+spdoc@users.noreply.github.com>
2019-03-01 20:33:16 +0530

    Updated

--- main_infra.sh
+++ main_infra.sh
@@ -9,7 +9,6 @@
 #curl -o /usr/local/bin/ecs-cli https://s3.amazonaws.com/amazon-ecs-cli/ecs-cli-linux-amd64-latest
 #chmod +x /usr/local/bin/ecs-cli
 #alias ecs-cli="/usr/local/bin/ecs-cli"
-mkdir Hackathon;cd Hackathon;
 #**Configure AWS**
 aws configure set default.region ap-south-1
 read -s -p "Enter Your access-key: "  ak;printf '\n';
